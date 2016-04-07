53991e907d580e5ea2438a68a0a2edc45363948e
Neyanbhbin <kanng.yongjie@gmail.com>
2016-04-07 10:44:40 -0500

    added linear svm

--- hw4/main.py
+++ hw4/main.py
@@ -1,2 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from __future__ import division
+from svm import SVM
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import svm, datasets
+
+
+def linearSVM():
+    iris = load_iris()
+    X = iris.data[:100, 1:3]
+    y = iris.target[:100]
+
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.xlabel("Sepal Width")
+    plt.ylabel("Petal Length")
+    plt.title("Iris")
+    plt.show()
+    x_min = int(min(X[:, 0])) - 1
+    x_max = int(max(X[:, 0])) + 1
+    s = SVM()
+    s.fit(X, y)
+    w, w_0, sp_idx = s.coef_
+
+    # print w, w_0, sp_idx
+
+    X_sp = X[sp_idx]
+    y_sp = iris.target[sp_idx]
+    plt.scatter(X_sp[:, 0], X_sp[:, 1], c="red")
+
+    x = np.array([i for i in range(x_min, x_max)])
+    y = (-w_0 - x*w[0,0]) / w[1, 0]
+    y_1 = (1-w_0 - x*w[0,0]) / w[1, 0]
+    y_m_1 = (-1-w_0 - x*w[0,0]) / w[1, 0]
+    plt.plot(x, y, label="Wx + W_0 = 0")
+    plt.plot(x, y_1, label="Wx + W_0 = 1")
+    plt.plot(x, y_m_1, label="Wx + W_0 = -1")
+    plt.legend(loc=0, borderaxespad=0.)
+    # plt.show()
+
+
+def nonlinearSVM():
+    data = pd.read_csv("dataset/fourclass.csv")
+    X = data[["x1", "x2"]].as_matrix()
+    m, n = X.shape
+    y = data["y"].as_matrix()
+
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    linearSVM()
