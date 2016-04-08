162f50b72b302ab1235a846ae05ebb292614f927
Neyanbhbin <kanng.yongjie@gmail.com>
2016-04-07 19:06:46 -0500

    added kernel SVM

--- hw4/main.py
+++ hw4/main.py
@@ -9,6 +9,10 @@ import numpy as np
 import pandas as pd
 from sklearn import svm, datasets
 
+'''
+Data source url:
+    http://openclassroom.stanford.edu/MainFolder/DocumentPage.php?course=MachineLearning&doc=exercises/ex8/ex8.html
+'''
 
 def linearSVM():
     iris = load_iris()
@@ -17,7 +21,8 @@ def linearSVM():
 
     plt.scatter(X[:, 0], X[:, 1], c=y)
     plt.xlabel("Sepal Width")
-    plt.ylabel("Petal Length")
+    # plt.ylabel("Petal Length")
+    plt.ylabel("Sepal Length")
     plt.title("Iris")
     x_min = int(min(X[:, 0])) - 1
     x_max = int(max(X[:, 0])) + 1
@@ -40,21 +45,58 @@ def linearSVM():
     plt.show()
 
 
+def radial_SVM():
+    iris = load_iris()
+    X = iris.data[:100, :2]
+    y = iris.target[:100]
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.xlabel("Sepal Width")
+    plt.ylabel("Petal Length")
+    plt.title("Iris")
+    s = SVM(kernel="radial", gamma=0.9)
+    s.fit(X, y)
+    x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
+    y_min, y_max = X[:, 1].min() - 5, X[:, 1].max() + 5
+    h = 0.02
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+    Z = s.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.9)
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.show()
+
+
 def nonlinearSVM():
     data = pd.read_csv("dataset/fourclass.csv")
     X = data[["x1", "x2"]].as_matrix()
     m, n = X.shape
     y = data["y"].as_matrix()
-    x_min = int(min(X[:, 0])) - 1
-    x_max = int(max(X[:, 0])) + 1
+    h = 0.02
 
-    s = SVM(kernel="polynomial")
-    s.fit(X, y)
-    plt.scatter(X[:, 0], X[:, 1], c=y)
-    # plt.show()
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
 
+    fig_idx = 1
+    for d in [10, 20, 30,40, 50, 60]:
+        s = SVM(kernel="polynomial", degree=1)
+        # s = svm.SVC(kernel='poly', degree=d, C=1.0).fit(X, y)
+        # s = svm.SVC(kernel='rbf', gamma=0.7, C=1.0).fit(X, y)
+        s.fit(X, y)
+        Z = s.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        # plt.subplot(3,2, fig_idx)
+        # plt.title("Degree=1" %d)
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+        plt.scatter(X[:, 0], X[:, 1], c=y)
+        fig_idx += 1
+        break
+    plt.show()
 
 
 if __name__ == "__main__":
-    # linearSVM()
-    nonlinearSVM()
+    linearSVM()
+    # radial_SVM()
+    # nonlinearSVM()
